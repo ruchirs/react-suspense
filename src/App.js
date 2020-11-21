e03d38b0a7258387d410f6edfb009f3fe715e061
Ruchir Singh <ruchirsingh@Ruchirs-MacBook-Pro.local>
2020-11-21 19:02:46 +0800

    updated user display with styles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { fetchData } from './providers/API'
 const resource = fetchData()
 
 const App = () => (
-    <div className="container">
+    <div className="container my-5">
       <Suspense fallback={<h1>loading users . . .</h1>}>
         <UserDetails />
       </Suspense>
@@ -15,12 +15,12 @@ const App = () => (
 const UserDetails = () => {
   const user = resource.user.read();
   return (
-    <div>
-      <h1>{user.name}</h1>
+    <div className='card card-body my-3'>
+      <h1 className='large text-primary'>{user.name}</h1>
       <ul>
-        <li>{user.name}</li>
-        <li>{user.email}</li>
-        <li>{user.address.city}</li>
+        <li>Username: {user.name}</li>
+        <li>Email: {user.email}</li>
+        <li>City: {user.address.city}</li>
       </ul>
     </div>
   )
